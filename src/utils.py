edb8b12eb0d44eee05dba81c8b3f16986a9c8fdf
dom <davide.domini@unibo.it>
2024-01-02 12:27:59 +0100

    feat: add util method to get the dataset

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import copy
+import torch
+from torchvision import datasets, transforms
+from sampling import mnist_noniid_unequal
+
+
+def get_dataset(name, num_users):
+    data_dir = '../data/mnist/'
+    apply_transform = transforms.ToTensor()
+
+    train_dataset = datasets.MNIST(data_dir,
+                                   train=True,
+                                   download=True,
+                                   transforms=apply_transform)
+
+    test_dataset = datasets.MNIST(data_dir,
+                                  train=False,
+                                  download=True,
+                                  transforms=apply_transform)
+
+    user_groups = mnist_noniid_unequal(train_dataset, num_users)
+
+    return train_dataset, test_dataset, user_groups
